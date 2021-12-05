7c933f5e2b38ffa8241cbddb6af6e93d3d9b3b39
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-12-05 20:07:58 -0300

    corrige porta

--- section-2/01.simple_server.js
+++ section-2/01.simple_server.js
@@ -4,4 +4,4 @@ const server = http.createServer((req, res) => {
   res.end('Hello from port 3000!');
 });
 
-server.listen(7000, '127.0.0.1', () => console.log('server running'));
+server.listen(3000, '127.0.0.1', () => console.log('server running'));
