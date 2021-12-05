3f2bf9c88305bab21bf25354c461d39fb3af62fe
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-12-05 19:58:57 -0300

    cria server simples

---
+++ section-2/01.simple_server.js
@@ -0,0 +1,7 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from port 3000!');
+});
+
+server.listen(7000, '127.0.0.1', () => console.log('server running'));
