948db72b2f108b7995bd563237ac5314f9e0f0ef
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-02-11 02:52:15 -0300

    add checkBody midware

--- section-4-natours/app.js
+++ section-4-natours/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 const toursRouter = require('./routes/toursRoutes');
 const userRouter = require('./routes/usersRoutes');
 
 // 1) SERVER
 const app = express();
 // 2) MIDDLEWARE
+app.use(morgan('dev'));
 app.use(express.json());
 // Custom middleware
 app.use((req, res, next) => {
