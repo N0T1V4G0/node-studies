83c7f8c3b502280e169c6519dd4fec1190113065
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-03-18 18:49:56 -0300

    cria tourSchema e Tour model

--- section-4-natours/server.js
+++ section-4-natours/server.js
@@ -21,6 +21,24 @@ mongoose
     console.log(conObj.connections);
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Missing tour name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Missing tour price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 port = 3000 || process.env.PORT;
 app.listen(port, () => {
   console.log(`App running in port ${port}!`);
