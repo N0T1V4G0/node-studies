c154e4f13894bda320821d1b06c17395f2382722
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-03-18 18:37:50 -0300

    feat: adiciona mongoose e conecta ao atlas

--- section-4-natours/server.js
+++ section-4-natours/server.js
@@ -1,8 +1,25 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 console.log(process.env.NODE_ENV);
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((conObj) => {
+    console.log('db running');
+    console.log(conObj.connections);
+  });
 
 port = 3000 || process.env.PORT;
 app.listen(port, () => {
